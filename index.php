188e9edda66656889407b49ec4532c5f987d5c93
ozankaze <ozankaze101@gmail.com>
2018-07-31 21:43:12 +0700

    prepare statement

--- index.php
+++ index.php
@@ -12,14 +12,22 @@ if( $mysqli->connect_errno ) {
 }
 
 // multi query
-$sql = "INSERT INTO murids (`nama`, `alamat`) VALUES ('seena', 'jepara');";
-$sql = "INSERT INTO murids (`nama`, `alamat`) VALUES ('budi', 'cianjur');";
+// $sql = "INSERT INTO murids (`nama`, `alamat`) VALUES ('seena', 'jepara');";
+// $sql = "INSERT INTO murids (`nama`, `alamat`) VALUES ('budi', 'cianjur');";
 
-if( $mysqli->query($sql) === true ) {
-    echo 'berhasil';
-} else {
-    echo 'gagal';
-}
+// if( $mysqli->query($sql) === true ) {
+//     echo 'berhasil';
+// } else {
+//     echo 'gagal';
+// }
+
+// prepare statement
+// $statement = $mysqli->prepare('INSERT INTO murids (`nama`, `alamat`) VALUES (?. ?)');
+// $statement->bind_param('ss', $nama, $alamat);
+
+// $nama = "mas mase";
+// $alamat = "utara";
+// $statement->execute(); 
 
 
 $mysqli->close();
