754b6d79c6bdabcbda33194c8e455f3a35b6ff9f
ozankaze <ozankaze101@gmail.com>
2018-07-31 21:10:10 +0700

    membuat data

--- index.php
+++ index.php
@@ -10,3 +10,14 @@ $mysqli = new Mysqli($host, $user, $pass, $db);
 if( $mysqli->connect_errno ) {
     echo 'gagal bang ada yang error' . $mysqli->connect_error;
 }
+
+$sql = "INSERT INTO murids (`nama`, `alamat`) VALUES ('fauzan', 'cirebon')";
+
+if( $mysqli->query($sql) === true ) {
+    echo 'berhasil';
+} else {
+    echo 'gagal';
+}
+
+
+$mysqli->close();
